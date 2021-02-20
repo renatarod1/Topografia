acaf8fe7d2e4268175f66a865a7daebaa77e1cea
Renata Rodrigues <ticare1@hotmail.com>
2021-02-20 16:06:40 -0300

    Projeto Inicial

---
+++ FormatarPontosGPS/FormatarPontosGPS/Entities/PontoGps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FormatarPontosGPS.Entities
+{
+    class PontoGps {
+        public int Ponto { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public string Descricao { get; set; }
+
+        public PontoGps() { }
+        public PontoGps(int ponto, double x, double y, double z, string descricao) {
+            Ponto = ponto;
+            X = x;
+            Y = y;
+            Z = z;
+            Descricao = descricao;
+        }
+    }
+}
---
+++ FormatarPontosGPS/FormatarPontosGPS/Program.cs
@@ -0,0 +1,71 @@
+﻿using FormatarPontosGPS.Entities;
+using System;
+using System.IO;
+
+namespace FormatarPontosGPS
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("******************** Formatação de Arquivos de Pontos GPS ********************");
+            Console.WriteLine();
+            Console.WriteLine("Para que o processamento ocorra corretamente, o arquivo de origem ");
+            Console.WriteLine("deve conter a seguinte formatação para cada linha: ");
+            Console.WriteLine();
+            Console.WriteLine("Exemplo: ");
+            Console.WriteLine(@"""72 7442110,7005 328560,1692 886,6528 """"AL DIVISA""""""");
+            Console.WriteLine();
+            Console.WriteLine("Após a formatação, o novo arquivo apresentará cada linha no seguinte formato: ");
+            Console.WriteLine("72 	7442110,7005 	328560,1692 	886,6528 	AL DIVISA");
+            Console.WriteLine();
+            Console.Write(" **** Digite o caminho do arquivo completo: ");
+            string sourceFilePath = Console.ReadLine();
+            string path = Path.GetDirectoryName(sourceFilePath);
+            string targetDirectoryPath = path + @"\Pontos_Formatados";
+
+            try {
+                Directory.CreateDirectory(targetDirectoryPath);
+                string targetFilePath = targetDirectoryPath + @"\Pontos_Formatados.txt";
+
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath)) {
+                    foreach (string line in lines) {
+                        string line1 = line.Trim('"');
+                        string[] item = line1.Split(' ');
+                        int ponto = int.Parse(item[0]);
+                        double x = double.Parse(item[1]);
+                        double y = double.Parse(item[2]);
+                        double z = double.Parse(item[3]);
+                        string descricao = item[4].TrimStart('"').TrimEnd('"');
+                        if (item.Length > 4) {
+                            for (int i = 5; i < item.Length; i++) {
+                                descricao += " " + item[i].TrimStart('"').TrimEnd('"');
+                            }
+                        }                        
+                        PontoGps pontoGps = new PontoGps(ponto, x, y, z, descricao);
+                        sw.WriteLine(                            
+                            pontoGps.Ponto + " " + "\t" +
+                            pontoGps.X.ToString("F4") + " " + "\t" +
+                            pontoGps.Y.ToString("F4") + " " + "\t" +
+                            pontoGps.Z.ToString("F4") + " " + "\t" +
+                            pontoGps.Descricao + " "
+                            );                     
+                    }
+                    Console.WriteLine("Operação Realizada com Sucesso!");
+                    Console.WriteLine();
+                    Console.WriteLine("Caminho do arquivo gerado: " + targetFilePath.ToString().ToUpper());
+                    Console.WriteLine("Pressione qualquer tecla para finalizar ...");
+                    Console.ReadKey();
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("Ocorreu o seguinte erro: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.WriteLine("Pressione qualquer tecla para finalizar ...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
